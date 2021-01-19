a130fb588c95bb7721aad0f27421585dac6939f3
TheOtherBloke <32566545+TheOtherBloke@users.noreply.github.com>
2021-01-19 14:51:28 +0000

    Explore The Neighbour

--- Assets/Pathfinder.cs
+++ Assets/Pathfinder.cs
@@ -7,11 +7,37 @@ public class Pathfinder : MonoBehaviour
 
     Dictionary<Vector2Int, Waypoint> grid = new Dictionary<Vector2Int, Waypoint>();
 
+    Vector2Int[] directions =
+    {
+        Vector2Int.up,
+        Vector2Int.right,
+        Vector2Int.down,
+        Vector2Int.left
+    };
+
     // Start is called before the first frame update
     void Start()
     {
         LoadBlocks();
         ColorStartAndEnd();
+        ExploreNeighbours();
+    }
+
+    private void ExploreNeighbours()
+    {
+        foreach (Vector2Int direction in directions)
+        {
+            Vector2Int explorationCoordinates = startWaypoint.GetGridPos() + direction;
+            print("Exploring " + explorationCoordinates);
+            try
+            {
+                grid[explorationCoordinates].SetTopColor(Color.blue);
+            }
+            catch
+            {
+                // do nothing
+            }
+        }
     }
 
     private void ColorStartAndEnd()
