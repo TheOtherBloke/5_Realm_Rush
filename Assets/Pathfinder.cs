038679215c5f94df7d45fb3a0d88015a3cc5fb8d
TheOtherBloke <32566545+TheOtherBloke@users.noreply.github.com>
2021-01-22 14:13:21 +0000

    Reversing A List

--- Assets/Pathfinder.cs
+++ Assets/Pathfinder.cs
@@ -7,10 +7,9 @@ public class Pathfinder : MonoBehaviour
 
     Dictionary<Vector2Int, Waypoint> grid = new Dictionary<Vector2Int, Waypoint>();
     Queue<Waypoint> queue = new Queue<Waypoint>();
-
     bool isRunning = true;
-
-    Waypoint searchCenter; // the current SearchCenter
+    Waypoint searchCenter;
+    List<Waypoint> path = new List<Waypoint>();
 
     Vector2Int[] directions =
     {
@@ -20,16 +19,31 @@ public class Pathfinder : MonoBehaviour
         Vector2Int.left
     };
 
-    // Start is called before the first frame update
-    void Start()
+    public List<Waypoint> GetPath()
     {
         LoadBlocks();
         ColorStartAndEnd();
-        Pathfind();
-        //ExploreNeighbours();
+        BreadthFirstSearch();
+        CreatePath();
+        return path;
     }
 
-    private void Pathfind()
+    private void CreatePath()
+    {
+        path.Add(endWaypoint);
+
+        Waypoint previous = endWaypoint.exploredFrom;
+        while (previous != startWaypoint)
+        {
+            path.Add(previous);
+            previous = previous.exploredFrom;
+        }
+
+        path.Add(startWaypoint);
+        path.Reverse();
+    }
+
+    private void BreadthFirstSearch()
     {
         queue.Enqueue(startWaypoint);
 
@@ -40,8 +54,6 @@ public class Pathfinder : MonoBehaviour
             ExploreNeighbours();
             searchCenter.isExplored = true;
         }
-
-        print("Finished pathfinding?");
     }
 
     private void HaltIfEndFound()
@@ -59,14 +71,11 @@ public class Pathfinder : MonoBehaviour
         foreach (Vector2Int direction in directions)
         {
             Vector2Int neighbourCoordinates = searchCenter.GetGridPos() + direction;
-            try
+            if (grid.ContainsKey(neighbourCoordinates))
             {
                 QueueNewNeighbours(neighbourCoordinates);
             }
-            catch
-            {
-                // do nothing
-            }
+
         }
     }
 
@@ -87,6 +96,7 @@ public class Pathfinder : MonoBehaviour
 
     private void ColorStartAndEnd()
     {
+        // todo consider moving out
         startWaypoint.SetTopColor(Color.green);
         endWaypoint.SetTopColor(Color.red);
     }
