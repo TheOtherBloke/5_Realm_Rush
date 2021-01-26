22b268e10697eb8e081c2159f4a65b2a9cc7b8b2
TheOtherBloke <32566545+TheOtherBloke@users.noreply.github.com>
2021-01-26 14:58:04 +0000

    Make Tower Shoot

---
+++ Assets/Scripts/EnemyDamage.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class EnemyDamage : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    private void OnParticleCollision(GameObject other)
+    {
+        print("I'm hit!");
+    }
+}
